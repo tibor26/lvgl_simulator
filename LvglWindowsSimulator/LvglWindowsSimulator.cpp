8e5d2a22b12dcadbe497f8bb95dc8d4ba87cf87a
tibor26 <tibor@namawell.com>
2025-03-05 10:56:10 +1100

    Add files via upload

---
+++ LvglWindowsSimulator/LvglWindowsSimulator.cpp
@@ -0,0 +1,129 @@
+﻿#include <Windows.h>
+
+#include <LvglWindowsIconResource.h>
+
+#include "lvgl/lvgl.h"
+#include "lvgl/examples/lv_examples.h"
+#include "lvgl/demos/lv_demos.h"
+#include "ui/ui.h"
+#include "ui/ui_app.h"
+
+
+// create a white circular border
+// to show the round shape of the screen
+void add_white_border(lv_obj_t* screen)
+{
+    lv_obj_t* obj = lv_obj_create(screen);
+    lv_obj_set_pos(obj, 0, 0);
+    lv_obj_set_size(obj, LV_PCT(100), LV_PCT(100));
+    lv_obj_set_style_pad_left(obj, 0, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_pad_top(obj, 0, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_pad_right(obj, 0, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_pad_bottom(obj, 0, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_bg_opa(obj, 0, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_border_width(obj, 0, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_outline_color(obj, lv_color_hex(0xff000000), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_outline_width(obj, 85, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_radius(obj, LV_RADIUS_CIRCLE, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_outline_pad(obj, 1, LV_PART_MAIN | LV_STATE_DEFAULT);
+}
+
+
+int main()
+{
+    lv_init();
+
+    /*
+     * Optional workaround for users who wants UTF-8 console output.
+     * If you don't want that behavior can comment them out.
+     *
+     * Suggested by jinsc123654.
+     */
+#if LV_TXT_ENC == LV_TXT_ENC_UTF8
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+#endif
+
+    int32_t zoom_level = 80;
+    bool allow_dpi_override = false;
+    bool simulator_mode = true;
+    lv_display_t* display = lv_windows_create_display(
+        L"LVGL Windows Simulator Display 1",
+        400,
+        400,
+        zoom_level,
+        allow_dpi_override,
+        simulator_mode);
+    if (!display)
+    {
+        return -1;
+    }
+
+    HWND window_handle = lv_windows_get_display_window_handle(display);
+    if (!window_handle)
+    {
+        return -1;
+    }
+
+    HICON icon_handle = LoadIconW(
+        GetModuleHandleW(NULL),
+        MAKEINTRESOURCE(IDI_LVGL_WINDOWS));
+    if (icon_handle)
+    {
+        SendMessageW(
+            window_handle,
+            WM_SETICON,
+            TRUE,
+            (LPARAM)icon_handle);
+        SendMessageW(
+            window_handle,
+            WM_SETICON,
+            FALSE,
+            (LPARAM)icon_handle);
+    }
+
+#if 0
+    lv_indev_t* pointer_indev = lv_windows_acquire_pointer_indev(display);
+    if (!pointer_indev)
+    {
+        return -1;
+    }
+#endif
+
+    lv_indev_t* encoder_indev = lv_windows_acquire_encoder_indev(display);
+    if (!encoder_indev)
+    {
+        return -1;
+    }
+
+    //lv_demo_widgets();
+    //lv_demo_benchmark();
+    ui_init();
+
+#if 1
+    lv_indev_t* keypad_indev = lv_windows_acquire_keypad_indev(display);
+    if (!keypad_indev)
+    {
+        return -1;
+    }
+    lv_indev_set_group(keypad_indev, lv_group_get_default());
+#endif
+
+    lv_indev_set_group(encoder_indev, lv_group_get_default());
+
+    // add white circular border to all screens
+    add_white_border(objects.nama);
+    add_white_border(objects.home);
+    add_white_border(objects.select);
+    add_white_border(objects.program);
+
+    ui_app_startup(encoder_indev);
+
+    while (1)
+    {
+        uint32_t time_till_next = lv_timer_handler();
+        Sleep(time_till_next);
+    }
+
+    return 0;
+}
